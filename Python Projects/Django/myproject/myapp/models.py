b7c179da5f46b6286d645458a70ea0d1626adb83
Evandita <evanditaw@gmail.com>
2024-01-22 21:52:17 +0700

    Django - Models

--- Python Projects/Django/myproject/myapp/models.py
+++ Python Projects/Django/myproject/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Weapon(models.Model):
+    name = models.CharField(max_length=100, default="Weapon")
+    attack = models.IntegerField(null=True)
+    price = models.IntegerField(null=True)
+    
--- Python Projects/Django/myproject/myapp/views.py
+++ Python Projects/Django/myproject/myapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Weapon
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    weapons = Weapon.objects.all()
+    
+    return render(request, 'index.html', {'weapons': weapons})
 
 def counter(request):
     text = request.POST['text']
